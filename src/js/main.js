5a54d9ecbbc0f38b6830b70b156c879b90068aae
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2022-07-14 18:20:45 +0700

    feat: overview box ui

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
-console.log("hello world")
+import { feelsCard, originalCard, pressure, uvCard, windCard } from './template.js'
+const search_input = document.getElementById("search-box");
+const mainCardDetails = document.getElementById("main-details");
+const mainWind = document.getElementById("main-wind");
+const mainPressure = document.getElementById("main-pressure");
+const mainFeels = document.getElementById("main-temp");
+const mainUV = document.getElementById("main-uv")
+
+search_input.addEventListener("input", async e => {
+  e.preventDefault()
+  const value = e.target.value;
+  await searchData(value);
+});
+
+async function searchData(city) {
+  const key = "ae2ade5033e9450198d64844220502";
+  const resp = await fetch("https://api.weatherapi.com/v1/current.json?key=ae2ade5033e9450198d64844220502&q=" + city + "&aqi=no");
+  const respData = await resp.json();
+  mainCardDetails.innerHTML =  originalCard(respData);
+  mainWind.innerHTML = windCard(respData);
+  mainPressure.innerHTML = pressure(respData);
+  mainFeels.innerHTML = feelsCard(respData);
+  mainUV.innerHTML = uvCard(respData);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await searchData("palembang")
+})
+
+
---
+++ src/js/template.js
@@ -0,0 +1,65 @@
+export const originalCard = weather => {
+  return `
+    <div class="card-details p-9 text-white rounded-lg my-5" style="background-image: url(src/img/card_details.png); background-size:cover; ">
+      <div class="row-1 day flex justify-between items-center pb-5">
+        <h3 class="text-xl font-medium">Today</h3>
+        <h3 class="font-light">${weather.current.last_updated}</h3>
+      </div>
+      <div class="row-2 specifc flex justify-between items-center pb-5">
+        <div class="text-info">
+          <h3 class="text-4xl font-semibold ">${weather.current.temp_c}<span class="text-lime ">°C</span></h3>
+          <p class="text-half-white text-lg">${weather.current.condition.text}</p>
+        </div>
+        <img src="${weather.current.condition.icon}" width="100" alt="partly cloudly">
+      </div>
+      <div class="row-3 location flex text-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#28E13B" class="bi bi-geo-alt-fill"
+          viewBox="0 0 16 16">
+          <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+        </svg>
+        &nbsp;
+        ${weather.location.name}, ${weather.location.country}
+      </div>
+    </div>
+  `
+}
+
+export const windCard = weather => {
+  return `
+    <img src="src/img/wind.png" width="38" height="38" alt="wind">
+    <div class="text pl-4">
+      <p class="text-light-gray font-light text-xs lg:text-sm">Wind Speed</p>
+      <h3 class="text-white font-semibold text-xl lg:text-2xl">${weather.current.wind_mph}/mph</h3>
+    </div>
+  `
+}
+
+export const pressure = weather => {
+  return `
+    <img src="src/img/waves.png" width="38" height="38" alt="wind">
+    <div class="text pl-4">
+      <p class="text-light-gray font-light text-xs lg:text-sm">Pressure</p>
+      <h3 class="text-white font-semibold text-xl lg:text-2xl">${weather.current.pressure_mb}/mb</h3>
+    </div>
+  `
+}
+
+export const feelsCard = weather => {
+  return`
+    <img src="src/img/temp.png" width="38" height="38" alt="wind">
+    <div class="text pl-4">
+      <p class="text-light-gray font-light text-xs lg:text-sm">Feels Like</p>
+      <h3 class="text-white font-semibold text-xl lg:text-2xl">${weather.current.feelslike_c}°C</h3>
+    </div>
+  `
+}
+
+export const uvCard = weather => {
+  return `
+    <img src="src/img/uv.png" width="38" height="38" alt="wind">
+    <div class="text pl-4">
+      <p class="text-light-gray font-light text-xs lg:text-sm">UV Index</p>
+      <h3 class="text-white font-semibold text-xl lg:text-2xl">${weather.current.uv}/mph</h3>
+    </div>
+  `
+}
